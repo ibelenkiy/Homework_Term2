74b4d2cce5ae2ed27087e72572518c9cab5d65fb
ibelenkiy <ibelenkiy@aol.com>
2015-03-25 09:27:37 -0400

    Isaac

---
+++ Queue/Queue.java
@@ -0,0 +1,29 @@
+public class myQueue<E>{
+    // You decide how the node(s) are declared
+    private Node<E> Back = null;
+    private Node<E> Front = null;
+    // make whatever constructor(s) you need
+    public myQueue<E>(){
+	myQueue<E> = new myQueue;
+	Back.setNext(Front);
+    }
+    public void enqueue(E data){
+        // add something to the tail/last
+myQueue<E> tmp = new my
+    }
+
+    public E dequeue(){
+        // remove and return the head/front item from the stack
+    }
+
+    public boolean empty(){
+
+    }
+
+    public E head() {
+        // return the first item in the queue
+    }
+}
+public class Queue<E>{
+
+    private Node<E> Back;
