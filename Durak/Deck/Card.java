98319564dd898337a55a4b6b2981a7376b361369
ibelenkiy <ibelenkiy@aol.com>
2015-05-13 09:26:22 -0400

    Deck Started

--- Durak/Deck/Card.java
+++ Durak/Deck/Card.java
@@ -1,16 +1,21 @@
 public class Card{
     public int Value;
     public String Suit;
-    int[] Values = [2,3,4,5,6,7,8,9,10,11,12,13,14];
-    String[] Suits = ["Hearts","Spades","Clubs","Diamonds"];
     public Card(int Value, String Suit){
 	Value = this.Value;
 	Suit = this.Suit;
     }
-    public int getValue(){
-	return Value;
+    public int getValue(){	
+	    return Value;
+
     } 
     public String getSuit(){
 	return Suit;
+    }
+    public String readCard(){
+	return " "+ Value + " of " + Suit;
+	    }
+    public int compareTo(Card c){
+	return Value - c.getValue();
     }
 }
